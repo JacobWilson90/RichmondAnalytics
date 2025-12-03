3da042952b5e329830184919569d291f1f70cf56
JacobWilson90 <134562222+JacobWilson90@users.noreply.github.com>
2025-12-03 11:05:26 +1100

    Commit Trial 2

---
+++ R/MetricCats.R
@@ -0,0 +1,45 @@
+#' Load all Champion Data
+#'
+#' @return A list of data frames: CS, CS1, CS2, CS3
+#' @export
+load_MetricCats <- function(
+    path = system.file("extdata", "Champion_Stats_V.xlsx", package = "RichmondAnalytics")
+) {
+
+  CS  <- readxl::read_excel(path)
+
+  CS1 <- readxl::read_excel(path) |>
+    dplyr::filter(
+      champion.stat.code %in% SSMET$Metric_Name |
+        champion.stat.code %in% c(
+          'KICK','EFFECTIVE KICK','SHOT AT GOAL','GOAL','BEHIND','RUSHED BEHIND',
+          'THROW IN HITOUT','BALL UP HITOUT','BU HITOUT TO ADVANTAGE',
+          'TI HITOUT TO ADVANTAGE','BALL UP HITOUT SHARKED',
+          'THROW IN HITOUT SHARKED','BALL UP FIRST POSSESSION',
+          'THROW IN FIRST POSSESSION','THROW IN CLEARANCE',
+          'BALL UP CLEARANCE','TACKLE','MISSED TACKLES'
+        )
+    )
+
+  CS2 <- readxl::read_excel(path) |>
+    dplyr::filter(
+      champion.stat.code %in% c(
+        "DISPOSAL","CENTRE BOUNCE","THROW IN","BALL UP","UNCONTESTED POSSESSION",
+        "CONTESTED POSSESSION","DISPOSAL","KICK IN","OUT ON FULL","FREE FOR",
+        "BEHIND","RUSHED BEHIND","SPOIL","INSIDE 50 RESULT","ONE","CONTEST TARGET",
+        "OUT OF BOUNDS","BALL UP CALL","GOAL","DROPPED MARK","NO PRESSURE ERROR",
+        "KNOCK ON","CENTRE BOUNCE","BALL UP","THROW IN"
+      )
+    )
+
+  CS3 <- readxl::read_excel(path) |>
+    dplyr::filter(
+      champion.stat.code %in% c(
+        'HITOUT','HIT OUT TO ADVANTAGE','HIT OUT SHARKED','FIRST POSSESSION',
+        'CLEARANCE','MARK','TACKLE','CONTESTED POSSESSION','GROUND BALL GET',
+        'MISSED TACKLES','FREE AGAINST'
+      )
+    )
+
+  list(CS = CS, CS1 = CS1, CS2 = CS2, CS3 = CS3)
+}
